1a423129cc135e5e3ce99f90b369eb89c9169512
Shiwen.Cao <43545349+StevenCao9988@users.noreply.github.com>
2019-07-01 22:57:06 +0800

    complete PID tool function

--- src/PID.cpp
+++ src/PID.cpp
@@ -13,13 +13,22 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
 
+	PID.Kp = Kp_;
+	PID.Ki = Ki_;
+	PID.Kd = Kd_;
+
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+	i_error = i_error + cte;
+	d_error = cte - p_error;
+	p_error = cte;
 }
 
 double PID::TotalError() {
